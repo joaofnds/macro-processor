1e67ea6f789d1ca564b5fbd75afb36cdc2106d3c
Carlos Calage <calage.carlos@gmail.com>
2021-09-17 15:18:12 -0300

    Added arraylist

--- Macro.java
+++ Macro.java
@@ -1,16 +1,19 @@
 import java.io.File;  
 import java.io.FileNotFoundException;  
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Macro { 
   public static void main(String[] args) {
+    ArrayList<String> asm = new ArrayList<String> ();
+
     try {
       File myObj = new File("teste_macro_z808.asm");
       Scanner myReader = new Scanner(myObj);
       while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
+        asm.add(myReader.nextLine());
       }
+      System.out.println(asm);
       myReader.close();
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
