6db792a088a06b744f329d67f32274189b186ada
Carlos Calage <calage.carlos@gmail.com>
2021-09-17 15:43:45 -0300

    Saving macro code

--- Macro.java
+++ Macro.java
@@ -5,16 +5,37 @@ import java.util.ArrayList;
 
 public class Macro { 
   public static void main(String[] args) {
-    ArrayList<String> asm = new ArrayList<String> ();
 
+    ArrayList<String> asm = new ArrayList<String> ();
+    ArrayList<String> macro = new ArrayList<String> ();
+
+    boolean storeMacro = false;
+
+    //Leitura do arquivo asm
     try {
       File myObj = new File("teste_macro_z808.asm");
       Scanner myReader = new Scanner(myObj);
+
       while (myReader.hasNextLine()) {
-        asm.add(myReader.nextLine());
+        String line = myReader.nextLine();
+        asm.add(line);
+
+        if(line.contains("MACRO")) {
+          storeMacro = true;
+        }
+
+        if(line.contains("ENDM")) {
+          storeMacro = false;
+        }
+
+        if(storeMacro) {
+          macro.add(line);
+        }
       }
-      System.out.println(asm);
+
+      System.out.println(macro);
       myReader.close();
+
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
