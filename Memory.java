555944d409e7c6e577abf2f5bfaeb903547bb510
João Fernandes <joaofnds@joaofnds.com>
2021-10-10 22:06:07 -0300

    add stack operations

--- Memory.java
+++ Memory.java
@@ -8,18 +8,22 @@
 |      Bitwise inclusive OR
 */
 
+import java.io.IOException;
+import java.util.BitSet;
+import java.util.Stack;
+
 public class Memory {
     private static final short MEM_SIZE = 2 << 12;
     private final short[] data = new short[MEM_SIZE];
-    private final short sp = 0;
     private final short si = 0;
-    private final boolean cf = false;
-    private final boolean pf = false;
-    private final boolean inf = false;
-    private final boolean sf = false;
-    private final boolean of = false;
     private final short highMask = (short) 0xff00;
     private final short lowMask = (short) 0x00ff;
+    private short sp = MEM_SIZE;
+    private boolean cf = false;
+    private boolean pf = false;
+    private boolean inf = false;
+    private boolean sf = false;
+    private boolean of = false;
     private short ip = 0;
     private boolean zf = false;
     private short dx = 0;
@@ -169,6 +173,110 @@ public class Memory {
         if (!sf) {
             ip = address;
         }
+    }
+
+    public void call(short address) {
+        stackPush(ip);
+        jmp(address);
+    }
+
+    public void ret() {
+        ip = stackPop();
+    }
+
+    public void hlt() {
+        System.exit(0);
+    }
+
+    public void pop(short address) {
+        data[address] = stackPop();
+    }
+
+    public void pop(String reg) {
+        switch (reg) {
+            case "AX":
+                ax = stackPop();
+                break;
+            case "DX":
+                dx = stackPop();
+                break;
+        }
+    }
+
+    public void popf() {
+        var value = stackPop();
+
+        cf = getBit(value, 0);
+        pf = getBit(value, 6);
+        inf = getBit(value, 7);
+        zf = getBit(value, 8);
+        sf = getBit(value, 9);
+        of = getBit(value, 12);
+    }
+
+    public void push(String reg) {
+        switch (reg) {
+            case "AX":
+                stackPush(ax);
+                break;
+            case "DX":
+                stackPush(dx);
+                break;
+        }
+    }
+
+    public void pushf() {
+        var sr = new BitSet(16);
+
+        sr.set(12, of);
+        sr.set(9, sf);
+        sr.set(8, zf);
+        sr.set(7, inf);
+        sr.set(6, pf);
+        sr.set(0, cf);
+
+        stackPush(bitSetToShort(sr));
+    }
+
+    private void write(short address) {
+        System.out.println(get(address));
+    }
+
+    private void read(short address) throws IOException {
+        var in = System.in.read();
+        data[address] = (short) in;
+    }
+
+    private short stackPop() {
+        if (sp == MEM_SIZE) {
+            throw new Error("empty stack");
+        }
+
+        sp += 1;
+        var stackValue = data[sp];
+        return stackValue;
+    }
+
+    private void stackPush(short opd) {
+        data[sp] = opd;
+        sp -= 1;
+    }
+
+    private boolean getBit(int n, int k) {
+        var bitValue = (n >> k) & 1;
+        return bitValue == 1;
+    }
+
+    private short bitSetToShort(BitSet bs) {
+        var result = (short) 0;
+
+        for (int i = 0; i < bs.length(); i++) {
+            var bitValue = (short) (bs.get(i) ? 1 : 0);
+            result = (short) (result ^ bitValue);
+            result = (short) (result << 1);
+        }
+
+        return result;
     }
 
     private void assertValidAddress(short address) {
